363dc401a0c34a3ec07ce3be48f8783f288f01c7
Piyush Chhoriya <piyushchhoriya98@gmail.com>
2025-05-09 19:36:23 -0400

    backend code initial commit

---
+++ url-shorter-sb/src/main/java/com/url/shortener/UrlShorterSbApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterSbApplication.class, args);
+	}
+
+}
